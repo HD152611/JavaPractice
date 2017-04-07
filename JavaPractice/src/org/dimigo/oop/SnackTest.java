83a7c6db844deef4163fe6e3c1641b7b2c391e92
samsung <samsung@DESKTOP-PSFAMNS>
2017-04-07 09:29:40 +0900

    크래커

---
+++ JavaPractice/src/org/dimigo/oop/SnackTest.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *    |_SnackTest
+ *   
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 4. 6.
+ * </pre>
+ *
+ * @author		:samsung
+ * @version		:1.0
+ */
+public class SnackTest {
+	public static void main(String[] args) {
+		Snack[] snacks = null;
+		snacks = new Snack[3];
+		snacks[0] = new Snack ("새우깡","농심",1100,2);
+		snacks[1] = new Snack ("콘칲","크라운",1200,1);
+		snacks[2] = new Snack ("허니버터칲","해태",1500,4);
+		int sum = 0;
+		for(int i = 0 ; i < 3 ; i ++){
+			sum += snacks[i].calcPrice();
+			System.out.println(snacks[i].toString());
+			System.out.println();
+		}
+		System.out.println("총 구매 금액 : "+String.format("%,d",sum)+"원");
+	}
+	
+}
