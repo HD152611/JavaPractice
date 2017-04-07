a5aad00d520de1f92f5ebb2c1bab1d992282bdef
samsung <samsung@DESKTOP-PSFAMNS>
2017-04-07 09:45:16 +0900

    .

--- JavaPractice/src/org/dimigo/oop/SnackTest.java
+++ JavaPractice/src/org/dimigo/oop/SnackTest.java
@@ -21,7 +21,7 @@ public class SnackTest {
 		snacks = new Snack[3];
 		snacks[0] = new Snack ("새우깡","농심",1100,2);
 		snacks[1] = new Snack ("콘칲","크라운",1200,1);
-		snacks[2] = new Snack ("허니버터칲","해태",1500,4);
+		snacks[2] = new Snack ("허니버터칩","해태",1500,4);
 		int sum = 0;
 		for(int i = 0 ; i < 3 ; i ++){
 			sum += snacks[i].calcPrice();
